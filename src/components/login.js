20689aaa5838d8e867798967da420f30d58da072
dhruviben <dh342773@dal.ca>
2021-06-07 21:45:50 -0300

    code 5709

---
+++ src/components/login.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+
+export default class Login extends Component {
+    render() {
+    	
+        return (
+
+        	
+            <form>
+            <div class="col-4 text-center">
+                <h3>Library</h3></div>
+
+             
+
+
+                <div className="form-group">
+                <div class="row">
+    				<div class="col-4 text-center">
+                    <label>Email address</label>
+                    <input type="email" className="form-control" placeholder="Enter email" />
+                </div>
+                </div>
+                </div>
+
+                <div className="form-group">
+                <div class="row">
+    				<div class="col-4 text-center">
+                    <label>Password</label>
+                    <input type="password" className="form-control" placeholder="Enter password" />
+                </div>
+                </div>
+                </div>
+
+                <div class="row">
+    				<div class="col-4 text-center">
+                <button type="submit" className="button">Submit</button>
+                <p className="forgot-password text-right">
+                     <a href="#">Forgot password?</a>
+                </p>
+                </div>
+                </div>
+          
+
+            </form>
+
+
+
+        );
+        
+
+
+    }
+}
